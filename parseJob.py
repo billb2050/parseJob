2d4c61c2d6b2451b6cbe06dfb8527a68b8996cdd
Bill Blasingim <billb2050@gmail.com>
2021-03-18 15:37:09 -0400

    Check if print file exists

--- parseJob.py
+++ parseJob.py
@@ -3,29 +3,27 @@
   This Python 3 CLI program reads the Hercules prt00e.txt from MVS
   (tk4- in my case) output which contains multiple jobs, back to back, 
   and splits each job out to it's own file. It was  written for use in
-  the Linux OS, although I see no reason it wouldn't work in MS-Windows,
-  with the appropriate changes.
+  the Linux OS. 
 
   It opens the print file, read-only and can run while Hercules is active.
   There is maybe a likelihood that there could be problems reading while
-  Hercules is writing. So it's probably best run between Job execution, 
-  however I've never had a problem.
+  Hercules is writing. So it's probably best run between Job execution.
 
   This program extracts the individual jobs into a "jobs" subdirectory, 
-  which It will create it if it doesn't exist in the "prt" directory.
+  which It will create it if it doesn't exist in the "prt" directory. .
 
-  Files are named "JobName-JobNo (date&time).txt"
+  Files are named "JobNo-JobName(date&time).txt"
   
   I had to add date & time because some Jobs with the same Job Name & 
   Job Number appear multiple times. Such as the "MF1" job. This program 
   makes some assumtions based on what was in my Hercules print file on 
   the day I wrote it. YMMV!
 
-  I store & run this program in Hercules "prt" subdirectory. It can run 
-  in a terminal, by typing './parseJob.py' in the Hercules prt directory, 
-  on my system you can also just double click it in the file manager within 
-  that directory. If you run it in a terminal it will provide record counts.
-  Obviously it must be set as executable. 
+  I store & run it in Hercules "prt" subdirectory. It can run in a terminal,
+  by typing './parseJob.py' in the Hercules prt directory, on my system you 
+  can also just double click it in the file manager within that directory. 
+  Obviously it must be set as executable. If you run it in a terminal it will
+  provide further information.
 
   It's fast
   Read a 7MB+, 80322 line prt file and wrote (extracted) 42 files (jobs)
@@ -40,26 +38,17 @@ prt00f.txt (SYSOUT=Z) in folder tk4-/prt.
 
   11/09/2019  Process prt00e and prt00f also.
               Change open because of special characters in prt00f
-  03/12/2020  Fix printed counts
-  03/21/2020  Some filename date formatting. Mostly because I don't like the alpha month!
+  02/27/2021  Fix 'Written' count
+  03/16/2021  Check if print file exists
   
 """
 import os, string, time
-# Create a list of short month names
-months=[]
-months=["January","February","March","April","May","June","July","August","September","October","November","December"]
-monthShort=months
-i=0
-for month in months:
-    mnth=month[:3].upper()  #Just 1st 3 month letters
-    #print(mnth)
-    monthShort[i]=mnth
-    i+=1
-
 cr=chr(13)	# Carriage return
 lf=chr(10)	# Line Feed
 crlf=cr+lf
 nl="\n"	# New line
+start=True
+end=False
 endCnt=0
 lastLine=0
 firstLine=1
@@ -67,10 +56,11 @@ jobs = {}
 #tmpFil=subdir+"tmp"+str(int(time.time()))+".txt"
 tmpFil="tmp"+str(int(time.time()))+".txt" #create temp workfile
 #CUUs=['00e','00f','002']
-#CUUs=['00f','00e']
 CUUs=['00e','00f']
 
 for cuu in CUUs:
+  start=True
+  end=False      
   FILEIN = "prt"+cuu+".txt"
   print("\nReading "+FILEIN)
   alpha='A'
@@ -78,15 +68,17 @@ for cuu in CUUs:
     alpha='Z'
 
   #fi = open(FILEIN, "r")
-  fi = open(FILEIN, "r", encoding = "ISO-8859-1")
+  try:
+    fi = open(FILEIN, "r", encoding = "ISO-8859-1")
+  except IOError:
+    print("File "+FILEIN+" does not exist!")
+    continue    
   # Create "jobs" subdirectory if it doesn't exist
   subdir="jobs"+"-"+cuu+"/"
   if not os.path.exists(subdir):
       os.makedirs(subdir)
 
   ln=0
-  start=True
-  end=False
   filCnt=0
   while 1:
     line = fi.readline()
@@ -104,21 +96,8 @@ for cuu in CUUs:
       job=LineOut[17:23]
       jobName=LineOut[24:33]
       timeDate=LineOut[67:88]
-
-      '''
-      Some date formatting
-      Mostly because I don't like a alpha month
-      '''
-      pt=timeDate.find(".",3)
-      tim=timeDate[:pt+6]
-      dat=timeDate[pt+7:]
-      mnth=dat[3:6]
-      idx=monthShort.index(mnth)+1   #Get month number
-      dat='20'+dat[7:9]+"-"+str(idx)+"-"+dat[:2]
-      #dat='20'+dat[7:9]+str(idx)+dat[:2]
-
       # File name = job# then job name
-      ofile=job.strip()+'-'+jobName.strip()+' ('+dat+' '+tim+').txt'
+      ofile=job.strip()+'-'+jobName.strip()+' ('+timeDate.strip()+').txt'
 
     fo.write(LineOut)
 
